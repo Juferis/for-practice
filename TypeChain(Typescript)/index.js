6f05166ab5ac3dad80a81a44dd2a07f741431e04
Juferis <66511872+Juferis@users.noreply.github.com>
2021-03-11 14:24:00 +0900

    func = (name, age, gender?)
    "?"를 달면 없어도 된다는 뜻이기에 실행이 가능해진다.

--- TypeChain(Typescript)/index.js
+++ TypeChain(Typescript)/index.js
@@ -1,2 +1,8 @@
-console.log("hello!");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var name = "Hyun", age = 26, gender = "male";
+var sayHello = function (name, age, gender) {
+    console.log("Hello! " + name + " you are " + age + " and you are " + gender);
+};
+sayHello(name, age, gender);
 //# sourceMappingURL=index.js.map
