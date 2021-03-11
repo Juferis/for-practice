551894dc62d24041a6b3c35e676e76de9a49b681
Juferis <66511872+Juferis@users.noreply.github.com>
2021-03-11 16:41:03 +0900

    update tsconfig

--- TypeChain(Typescript)/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var name = "Hyun", age = 26, gender = "male";
-var sayHello = function (name, age, gender) {
-    console.log("Hello! " + name + " you are " + age + " and you are " + gender);
-};
-sayHello(name, age, gender);
-//# sourceMappingURL=index.js.map
--- TypeChain(Typescript)/src/index.ts
+++ TypeChain(Typescript)/src/index.ts
@@ -2,11 +2,11 @@ const name = "Hyun",
   age = 26,
   gender = "male";
 
-const sayHello = (name, age, gender?) => {
+const sayHello = (name: string, age: number, gender?: string): string => {
   // ?를 달면 없어도 된다는 뜻이기에 실행이 가능해진다.
-  console.log(`Hello! ${name} you are ${age} and you are ${gender}`);
+  return `Hello! ${name} you are ${age} and you are ${gender}!`;
 };
 
-sayHello(name, age);
+console.log(sayHello(name, age, gender));
 
 export {}; // 위 코드들이 모듈이 된다고 알려주는 typescript 규칙
