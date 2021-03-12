dd77597a0c604430012a0c0223e5890343021b1d
Juferis <66511872+Juferis@users.noreply.github.com>
2021-03-12 14:36:27 +0900

    TS use class for JS

--- TypeChain(Typescript)/src/index.ts
+++ TypeChain(Typescript)/src/index.ts
@@ -1,14 +1,16 @@
-interface Human {
-  name: string;
-  age: number;
-  gender: string;
+class Human {
+  public name: string;
+  public age: number;
+  public gender: string;
+  constructor(name: string, age: number, gender: string) {
+    // 클래스가 시작할 때마다 호출되는 method
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
 }
 
-const person = {
-  name: "Hyun Jin",
-  age: 26,
-  gender: "male",
-};
+const person = new Human("Hyun Jin", 26, "male");
 
 const sayHello = (person: Human): string => {
   // person 오브젝트를 Human이라는 인터페이스 정의로 넣는다.
