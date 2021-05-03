9aa191813e6f09b99847ae693e454de461652c29
Juferis <juferis@naver.com>
2021-05-03 16:43:45 +0900

    handle key prop error

--- moive_app/src/App.js
+++ moive_app/src/App.js
@@ -1,29 +1,33 @@
-function Food({ name, picture }) {
-  // props.name == { name }
-  return (
-    <div>
-      <h1>I like {name}</h1>
-      <img src={picture} />
-    </div>
-  );
-}
-
 const favFood = [
   {
+    id: 1,
     name: "Kimchi",
     Image:
       "http://gdimg1.gmarket.co.kr/goods_image2/exlarge_moreimg/150/528/1505282345/1505282345_01.jpg?ver=1618280554",
   },
   {
+    id: 2,
     name: "Pork_Belly",
     Image: "https://cdn.imweb.me/thumbnail/20200324/876cb6cbe8132.jpg",
   },
   {
+    id: 3,
     name: "Icecream",
     Image:
       "https://lh3.googleusercontent.com/proxy/8oLoERKVQnrUW6uzuMFPbMS5YH-JtbqnZZDbjZWo0o2PMTZI7EQZZRdjgiGrj1le6S8s0q6m_ZYdYbbTDOgOkZ73B96UTGCqldDLhFTCmgRh5wJj4HM",
   },
 ];
+
+function Food({ name, picture }) {
+  // props.name == { name }
+  return (
+    <div>
+      <h1>I like {name}</h1>
+      <img src={picture} alt={name} />
+    </div>
+  );
+}
+
 function App() {
   return (
     <div>
@@ -31,7 +35,7 @@ function App() {
       {favFood.map((
         choice // choice는 object다
       ) => (
-        <Food name={choice.name} picture={choice.Image} />
+        <Food key={choice.id} name={choice.name} picture={choice.Image} />
       ))}
     </div>
   );
