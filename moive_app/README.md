829144fe859b1e6b49742f347ee8ddad112f56e8
Juferis <juferis@naver.com>
2021-05-03 18:06:08 +0900

    npm i prop-types

--- moive_app/README.md
+++ moive_app/README.md
@@ -1,9 +1,9 @@
 # ReactJS로 Movie app 만들기
 
-### 참고작업
+## 1.0 참고작업
 
 > 콘솔에 명령어 입력으로 설치 : npx create-react-app 폴더명  
-> git명령어 사용법
+> **git명령어 사용법**
 >
 > 1. git add .
 > 2. git commit -m "커밋제목"
@@ -16,22 +16,35 @@
 > 2. .env파일에 CHOKIDAR_USEPOLLING=true 입력하기
 > 3. 다시 npm start를 통해 시작해주면 수정 후 저장할 때마다 자동갱신해서 보여준다.
 
-## 새로 알게된 내용
+## 2.0 새로 알게된 내용
 
-### map 함수([강의](https://nomadcoders.co/react-fundamentals/lectures/1549))
+### 2.1 map 함수([강의](https://nomadcoders.co/react-fundamentals/lectures/1549))
 
 > Array의 각 item에 function을 적용하고 array를 돌려준다.
 > 무엇이 들어오든 반환값은 array이다.  
-> 예시)  
+> **예시)**  
 > ![img](./images/01_map.jpg)
 
-## 만난 에러와 처리 방법
+### 2.2 prop-types([참고](https://ko.reactjs.org/docs/typechecking-with-proptypes.html))
 
-### "key" prop에러(map함수 사용시 나왔다)
+> 설치 : npm i prop-types  
+> 설명 : 전달 받은 props가 내가 원하는 props인지를 확인해 준다.  
+> **예시)**  
+> ![img](./images/propTypes01.jpg)
+>
+> rating은 number타입이고 검사할 때 내가 원하는 값은 string이라고 입력한 후 결과
+>
+> ![img](./images/propTypes02.jpg)
+>
+> 에러를 통해 알려준다.
+
+## 3.0 만난 에러와 처리 방법
+
+### 3.1 "key" prop에러(map함수 사용시 나왔다)
 
 > ![img](./images/error_01.jpg)  
 > React의 각각 list 내의 child는 unique한 keyprop을 가져야하는데 갖지 않아 생기는 에러  
 > React의 element들은 unique해야 하고 list에 집어 넣을 때 유일성을 잃어버린다.
 
-> 해결 방법  
+> **해결 방법**  
 > 각각의 item에 id를 할당해 유일성을 주고 map함수 처리시 id를 같이 넘겨주는 방식으로 해결
--- moive_app/src/App.js
+++ moive_app/src/App.js
@@ -1,32 +1,44 @@
+import PropTypes from "prop-types";
+
 const favFood = [
   {
     id: 1,
     name: "Kimchi",
     Image:
       "http://gdimg1.gmarket.co.kr/goods_image2/exlarge_moreimg/150/528/1505282345/1505282345_01.jpg?ver=1618280554",
+    rating: 4.9,
   },
   {
     id: 2,
     name: "Pork_Belly",
     Image: "https://cdn.imweb.me/thumbnail/20200324/876cb6cbe8132.jpg",
+    rating: 4.8,
   },
   {
     id: 3,
     name: "Icecream",
     Image:
       "https://lh3.googleusercontent.com/proxy/8oLoERKVQnrUW6uzuMFPbMS5YH-JtbqnZZDbjZWo0o2PMTZI7EQZZRdjgiGrj1le6S8s0q6m_ZYdYbbTDOgOkZ73B96UTGCqldDLhFTCmgRh5wJj4HM",
+    rating: 4.5,
   },
 ];
 
-function Food({ name, picture }) {
+function Food({ name, picture, rating }) {
   // props.name == { name }
   return (
     <div>
       <h1>I like {name}</h1>
       <img src={picture} alt={name} />
+      <h2>Rating : {rating}/5.0</h2>
     </div>
   );
 }
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired,
+};
 
 function App() {
   return (
@@ -35,7 +47,12 @@ function App() {
       {favFood.map((
         choice // choice는 object다
       ) => (
-        <Food key={choice.id} name={choice.name} picture={choice.Image} />
+        <Food
+          key={choice.id}
+          name={choice.name}
+          picture={choice.Image}
+          rating={choice.rating}
+        />
       ))}
     </div>
   );
