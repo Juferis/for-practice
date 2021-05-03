8c429612d9cae0cf30672d0ebeffbc1c39877846
Juferis <juferis@naver.com>
2021-05-03 14:49:38 +0900

    #2.1 Components with JSX + Props

--- moive_app/src/App.js
+++ moive_app/src/App.js
@@ -1,5 +1,18 @@
+function Food({ fav }) {
+  // props.fav == { fav }
+  return <h3>I like {fav}</h3>;
+}
+
 function App() {
-  return <h1 className="App">Hello ReactJS!</h1>;
+  return (
+    <div>
+      <h1 className="App">Hello ReactJS!</h1>
+      <Food fav="kimchi" />
+      <Food fav="hambuger" />
+      <Food fav="skittles" />
+      <Food fav="meet" />
+    </div>
+  );
 }
 
 export default App;
--- moive_app/src/test.js
+++
@@ -1,5 +0,0 @@
-function Test() {
-  return <h4>This is Test</h4>;
-}
-
-export default Test;
