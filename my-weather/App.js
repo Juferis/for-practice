52b57212375f3acddc0dc5324f28bbf7ac5d3b8e
Juferis <66511872+Juferis@users.noreply.github.com>
2021-05-05 15:23:36 +0900

    getting latitude, longitude

--- my-weather/App.js
+++ my-weather/App.js
@@ -1,16 +1,28 @@
 import React from "react";
 import Loading from "./Loading";
 import * as Location from "expo-location";
+import { Alert } from "react-native";
 
 export default class extends React.Component {
+  state = {
+    isLoading: true,
+  };
   getLocation = async () => {
-    const location = await Location.getCurrentPositionAsync(option);
-    console.log(location);
+    try {
+      await Location.getForegroundPermissionsAsync();
+      const {
+        coords: { latitude, longitude },
+      } = await Location.getCurrentPositionAsync();
+      console.log(latitude, longitude);
+    } catch (error) {
+      Alert.alert("위치정보를 가져올 수 없습니다.", "위치정보를 허용해주세요");
+    }
   };
   componentDidMount() {
     this.getLocation();
   }
   render() {
-    return <Loading />;
+    const { isLoading } = this.state;
+    return isLoading ? <Loading /> : null;
   }
 }
