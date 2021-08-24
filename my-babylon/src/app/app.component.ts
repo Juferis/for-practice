5cb5e92ee64af8b133754729f38f53b637879310
hyunjin.kang <hyunjin.kang.igikorea@gmail.com>
2021-08-24 14:46:05 +0900

    create town

--- my-babylon/src/app/app.component.ts
+++ my-babylon/src/app/app.component.ts
@@ -18,7 +18,7 @@ export class AppComponent implements OnInit {
   public canvas!: HTMLCanvasElement;
   public engine!: BABYLON.Engine;
   public scene!: BABYLON.Scene;
-  public camera!: BABYLON.DeviceOrientationCamera;
+  public camera!: BABYLON.FreeCamera;
 
   @ViewChild('rendererCanvas', { static: true })
   public rendererCanvas!: ElementRef<HTMLCanvasElement>;
@@ -33,35 +33,38 @@ export class AppComponent implements OnInit {
     ));
 
     const scene = (this.scene = new BABYLON.Scene(engine));
+    scene.clearColor = new BABYLON.Color4(0.1, 0.1, 0.1, 0.5);
 
-    const camera = new BABYLON.DeviceOrientationCamera(
+    // sound
+    // const sound = new BABYLON.Sound('', 'assets/sounds/NCS.mp3', scene, null, {
+    //   loop: true,
+    //   autoplay: true,
+    // });
+
+    const camera = (this.camera = new BABYLON.FreeCamera(
       'camera',
-      new BABYLON.Vector3(0, 0, 0),
+      new BABYLON.Vector3(0, 50, 0),
       scene
-    );
-    camera.setTarget(new BABYLON.Vector3(0, 0, 10));
-    camera.attachControl(this.canvas, true);
-
-    // const camera = (this.camera = new BABYLON.FreeCamera(
-    //   'camera',
-    //   new BABYLON.Vector3(0, 50, 0),
-    //   scene
-    // ));
+    ));
 
     const light = new BABYLON.HemisphericLight(
       'light',
       new BABYLON.Vector3(0, 50, 0),
       scene
     );
-    var ground = BABYLON.MeshBuilder.CreateGround(
+    const ground = BABYLON.MeshBuilder.CreateGround(
       'ground',
-      { width: 14, height: 38 },
+      { width: 25, height: 80 },
       scene
     );
-    ground.position.x = +0.3;
-    ground.position.z = -1.4;
+    ground.position.z = -35.5;
+    const groundMat = new BABYLON.StandardMaterial('groundMat', this.scene);
+    groundMat.diffuseTexture = new BABYLON.Texture(
+      'assets/lib/images-png-road-9.png',
+      this.scene
+    );
+    ground.material = groundMat;
   }
-
   // @@이 친구를 잘 봐두자@@ 생성한 화면을 보여주기
   private runRenderLoop(): void {
     this.ngZone.runOutsideAngular(() => {
@@ -83,6 +86,76 @@ export class AppComponent implements OnInit {
       });
     });
   }
+  window1 = new BABYLON.Vector4(0.5, 0.0, 0.75, 1.0); //뒷 면
+  window2 = new BABYLON.Vector4(0.0, 0.0, 0.25, 1.0); //앞 면
+  window3 = new BABYLON.Vector4(0.25, 0, 0.5, 1.0); // 오른쪽
+  window4 = new BABYLON.Vector4(0.75, 0, 1.0, 1.0); //왼쪽
+  faceUV = [this.window1, this.window2, this.window3, this.window4];
+
+  public createBox() {
+    const box = BABYLON.MeshBuilder.CreateBox('box', {
+      size: 5,
+      height: 5,
+      depth: 5,
+      faceUV: this.faceUV,
+      wrap: true,
+    });
+    box.position.y = 2.5;
+    const boxMat = new BABYLON.StandardMaterial('boxMaterial', this.scene);
+    boxMat.diffuseTexture = new BABYLON.Texture(
+      'assets/lib/cubehouse.png',
+      this.scene
+    );
+    box.material = boxMat;
+
+    return box;
+  }
+
+  public createRoof() {
+    const roof = BABYLON.MeshBuilder.CreateCylinder('roof', {
+      diameter: 7,
+      height: 6,
+      tessellation: 3,
+    });
+    roof.scaling.x = 0.75;
+    roof.rotation.z = Math.PI / 2;
+    roof.position.y = 6;
+
+    const roofMat = new BABYLON.StandardMaterial('roofMat', this.scene);
+    roofMat.diffuseTexture = new BABYLON.Texture(
+      'https://assets.babylonjs.com/environments/roof.jpg',
+      this.scene
+    );
+    roof.material = roofMat;
+
+    return roof;
+  }
+
+  public createHouse() {
+    const house = BABYLON.Mesh.MergeMeshes(
+      [this.createBox(), this.createRoof()],
+      true,
+      false,
+      undefined,
+      false,
+      true
+    );
+
+    return house;
+  }
+
+  public createHouses() {
+    for (let i = 0; i < 10; i++) {
+      let house = this.createHouse();
+      house!.position.z = i * -8;
+      house!.position.x = -15;
+    }
+    for (let i = 0; i < 10; i++) {
+      let house = this.createHouse();
+      house!.position.z = i * -8;
+      house!.position.x = 15;
+    }
+  }
 
   // 메쉬를 가져와 화면에 띄워주기
   public importMeshes1() {
@@ -91,26 +164,23 @@ export class AppComponent implements OnInit {
       'assets/lib/',
       'shinwoo_level1.glb',
       this.scene,
-      () => {
-        this.scene.createDefaultCamera(true, true, true);
-      }
+      () => {}
     );
   }
 
-  // public importMeshes2() {
-  //   BABYLON.SceneLoader.ImportMesh(
-  //     '',
-  //     'assets/lib/',
-  //     'shinwoo_level2.glb',
-  //     this.scene,
-  //     () => {
-  //       this.scene.createDefaultCamera(true, true, true);
-  //     }
-  //   );
-  // }
+  public importMeshes2() {
+    BABYLON.SceneLoader.ImportMesh(
+      '',
+      'assets/lib/',
+      'shinwoo_level2.glb',
+      this.scene,
+      () => {}
+    );
+  }
   async ngOnInit() {
     this.runRenderLoop();
-    this.importMeshes1();
+    this.createHouses();
     // this.importMeshes2();
+    this.scene.createDefaultCamera(true, true, true);
   }
 }
