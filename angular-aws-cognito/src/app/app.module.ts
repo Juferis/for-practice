49950d49eee246b5b683dc53c4f858b74923ebdf
Juferis <66511872+Juferis@users.noreply.github.com>
2022-03-13 23:03:30 +0900

    cognito setting

--- angular-aws-cognito/src/app/app.module.ts
+++ angular-aws-cognito/src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { SignInComponent } from './auth/sign-in/sign-in.component';
+import { SignUpComponent } from './auth/sign-up/sign-up.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AuthService } from './auth/auth.service';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignInComponent,
+    SignUpComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  exports: [RouterModule],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- angular-aws-cognito/src/environments/environment.ts
+++ angular-aws-cognito/src/environments/environment.ts
@@ -3,8 +3,12 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+   cognitoUserPoolId: "<your_cognito_user_pool_id>",
+    cognitoAppClientId: "<your_cognito_app_client_id>"
 };
+
+
 
 /*
  * For easier debugging in development mode, you can import the following file
